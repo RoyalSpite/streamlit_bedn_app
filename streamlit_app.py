904716d5377e0363c5eead1ec5d9906b8c258ed1
Siriwut Longsakul <74346677+RoyalSpite@users.noreply.github.com>
2024-03-18 13:21:05 +0700

    complete

--- streamlit_app.py
+++ streamlit_app.py
@@ -11,23 +11,19 @@ import numpy as np
 
 @st.cache_resource
 def init_connection():
-    uri = f'mongodb+srv://{st.secrets["db_username"]}:{st.secrets["db_pswd"]}@image-test.kv0abp0.mongodb.net/?retryWrites=true&w=majority&appName=image-test'
-    return MongoClient(uri, server_api=ServerApi('1'))
+    global img_array
+    return MongoClient({st.secrets["mongo"]["uri"]}, server_api=ServerApi('1'))
 
 client = init_connection()
 
-@st.cache_data(ttl=600)
-def query(date = dt.date.today(), hour = 0, allday = False):
+@st.cache_data
+def query(date = dt.date.today()):
     
-    database = client['img']
-    collection = database['img']
+    database = client[st.secrets["mongo"]["col"]]
+    collection = database[st.secrets["mongo"]["col"]]
     
     _from = dt.datetime.strptime(str(date), '%Y-%m-%d')
-    if not allday:
-        _from = _from.replace(hour = hour)
-        _to = _from.replace(minute=59,second=59)
-    else:
-        _to = _from.replace(hour=23,minute=59,second=59)
+    _to = _from.replace(hour=23,minute=59,second=59)
         
     criteria = { "date": {"$gte": _from, "$lte": _to } }
 
@@ -55,7 +51,7 @@ with col1:
             format = "DD.MM.YYYY",
         )
         
-        AllDay = st.toggle('ดูตลอดทั้งวัน')
+        AllDay = st.toggle('ดูตลอดทั้งวัน', value=False)
             
         time = st.slider("เลือกช่วงชั่วโมง",
             min_value=0,max_value=23,step=1, value=0, 
@@ -69,17 +65,30 @@ with col1:
         # t = st.time_input('เลือกช่วงเวลา', dt.datetime.now())
             
 with col2:
-    img_set = st.container(border=True)
     
-    img_array = query(date, time, AllDay)
+    img_array = query(date)
+    filtered_array = list()
     
-    if len(img_array) == 0:
-        img_set.text("ไม่พบข้อมูล")
+    if AllDay:
+        filtered_array = img_array
+    else:           
+        _datetime_from = dt.datetime.strptime(str(date), '%Y-%m-%d').replace(hour=time)
+        _datetime_to = _datetime_from.replace(minute=59,second=59)
+            
+        filtered_array = list(filter(
+            lambda elem: (elem['date'] >= _datetime_from) and (elem['date'] <= _datetime_to),
+            img_array
+        ))
+            
+    if len(filtered_array) == 0:
+        st.container(border=True).write("ไม่มีข้อมูล")
     else:
-        for img_data in img_array:            
+        for img_data in filtered_array:            
+
             disp_date = img_data['date']
+                
             disp_text = f'{disp_date.day}/{disp_date.month}/{disp_date.year + 543}'
             disp_time = f'เวลา {disp_date.hour}:{disp_date.minute}:{disp_date.second}'
-                
+                    
             with st.expander(f'{disp_text} {disp_time}'):
                 st.image(img_data['img'])         
